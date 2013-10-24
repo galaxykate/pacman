1465da9d4b6b6abc1cb877f1da5c5411cd59680c
Kate <galaxykate@gmail.com>
2013-10-23 19:36:34 -0700

    Added up and down actions

---
+++ src/edu/ucsc/gameAI/GoDownAction.java
@@ -0,0 +1,24 @@
+package edu.ucsc.gameAI;
+
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+import edu.ucsc.gameAI.decisionTrees.binary.IBinaryNode;
+
+public class GoDownAction implements IAction, IBinaryNode {
+
+	public void doAction() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public IAction makeDecision(Game game) {
+		return this;
+	}
+
+	@Override
+	public MOVE getMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
