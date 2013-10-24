6816b60a7730c935e32598650d71e4d485eefdc6
Kate <galaxykate@gmail.com>
2013-10-23 19:47:28 -0700

    Adding advanced conditions

---
+++ src/edu/ucsc/gameAI/conditions/PowerPillInRegion.java
@@ -0,0 +1,21 @@
+package edu.ucsc.gameAI.conditions;
+
+import pacman.game.Game;
+import edu.ucsc.gameAI.ICondition;
+
+public class PowerPillInRegion implements ICondition {
+	private int x1, y1, x2, y2;
+
+	public PowerPillInRegion(int x1, int y1, int x2, int y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	@Override
+	public boolean test(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
