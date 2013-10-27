af6ddd9c75a0dcf839541350de1575d309ccab1c
Kate <galaxykate@gmail.com>
2013-10-27 12:23:02 -0700

    Adding pacman options

--- src/pacman/entries/pacman/KatesPacMan.java
+++ src/pacman/entries/pacman/KatesPacMan.java
@@ -6,6 +6,7 @@ import edu.ucsc.gameAI.decisionTrees.binary.BinaryDecision;
 import pacman.controllers.Controller;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
+import pacman.game.GameView;
 
 /*
  * This is the class you need to modify for your entry. In particular, you need to
@@ -14,19 +15,29 @@ import pacman.game.Game;
  */
 public class KatesPacMan extends Controller<MOVE> {
 	private BinaryDecision startNode;
+	private int[] powerPillNodes;
 
 	public KatesPacMan() {
 
 		// Build a tree
 		startNode = new BinaryDecision();
 
+		// Is there a very close ghost? Then move away
+		
+
 		startNode.setCondition(new CurrentLevelTime(100, 200));
 		startNode.setTrueBranch(new GoAction(MOVE.LEFT));
 		startNode.setFalseBranch(new GoAction(MOVE.RIGHT));
 	}
 
+	public void setPowerPillNodes(Game game) {
+		powerPillNodes = game.getActivePillsIndices();
+
+	}
+
 	@Override
 	public MOVE getMove(Game game, long timeDue) {
-		return startNode.makeDecision(game).getMove();
+		//GameView.addLines(game, color, fromNnodeIndex, toNodeIndex);
+		return startNode.makeDecision(game).getMove(game);
 	}
 }
